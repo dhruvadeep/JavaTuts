89f63a9011c64680f73aa8c74a81b094ff19f583
Dhruvadeep Malakar <57034827+dhruvadeep@users.noreply.github.com>
2020-12-11 12:44:25 +0530

    Add files via upload

---
+++ src/com/dhruvadeep/introduction/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.dhruvadeep.introduction.helloworld;  //Package => Indicates the path directory
+
+public class HelloWorld {
+    //every java class needs a class.
+    //HelloWorld is the name of the class and public means it can be used anytime
+    public static void main(String[] args) {
+        // System => system
+        // out => output
+        // println => Hello World !!
+
+        //CODE GOES HERE
+        System.out.println("Hello World!!");
+    } //End of main function
+} //End of class HelloWorld
+
+
+/*
+*   Every valid Java Application must have a class definition (that matches the filename).
+*   The main method must be inside the class definition.
+*   The compiler executes the codes starting from the main function.
+*/
