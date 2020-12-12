4564ec5a3dfb0ad2e1168073d4b92e3c61f2ac4b
Dhruvadeep Malakar <57034827+dhruvadeep@users.noreply.github.com>
2020-12-12 13:06:28 +0530

    Add files via upload

---
+++ src/com/dhruvadeep/jdkjrejvm/JdkJreJvm.java
@@ -0,0 +1,28 @@
+//        JVM (Java Virtual Machine) is an abstract machine that enables your computer to run a Java program.
+//
+//        When you run the Java program, Java compiler first compiles your Java code to bytecode. Then, the JVM
+//        translates bytecode into native machine code (set of instructions that a computer's CPU executes directly).
+//
+//        Java is a platform-independent language. It's because when you write Java code, it's ultimately written for
+//        JVM but not your physical machine (computer). Since JVM ​executes the Java bytecode which is platform-independent,
+//        Java is platform-independent.
+
+
+//        JAVA PROGRAM =(JAVA COMPILER)=> JAVA BYTECODE =(JVM)=> MACHINE CODE =(CPU)=> OUTPUT
+
+
+
+
+
+//JRE (Java Runtime Environment) is a software package that provides Java class libraries,
+// Java Virtual Machine (JVM), and other components that are required to run Java applications.
+
+//JRE is the superset of JVM.
+
+
+
+
+//JDK (Java Development Kit) is a software development kit required to develop applications in Java. When you download
+//JDK, JRE is also downloaded with it.
+
+//In addition to JRE, JDK also contains a number of development tools (compilers, JavaDoc, Java Debugger, etc).
